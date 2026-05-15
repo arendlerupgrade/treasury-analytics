a178f673765e3305e2b2a1aed2a5ed14a4c2f13a
arendlerupgrade <arendler@upgrade.com>
2026-05-15 16:47:58 -0600

    chore: remove test documentation for CRB Collateral Reporting

--- solutions/crb-collateral-reporting/Custom SQL Query1.sql
+++
@@ -1,8 +0,0 @@
--- Solution: CRB Collateral Reporting
--- Datasource: Custom SQL Query (core)
--- Query: Custom SQL Query1
-
-SELECT "crb_collateral_hi_oid"."effective_date" AS "effective_date",
-  "crb_collateral_hi_oid"."hi_oid" AS "hi_oid"
-FROM "dw_fbo_analytics"."crb_collateral_hi_oid" "crb_collateral_hi_oid"
-where effective_date <<= getdate()
